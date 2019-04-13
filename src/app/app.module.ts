e6852ad987fc8bb63ef3f6a1834f737e68e76930
Varun Sukheja <>
2019-04-13 16:57:54 +0530

    created new component: Radial Progress Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RadialProgressBarComponent } from './progress-bar/radial-progress-bar/radial-progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RadialProgressBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/progress-bar/radial-progress-bar/radial-progress-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radial-progress-bar',
+  templateUrl: './radial-progress-bar.component.html',
+  styleUrls: ['./radial-progress-bar.component.scss']
+})
+export class RadialProgressBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
