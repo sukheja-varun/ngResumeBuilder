aac732949ed2ef20c73f9e41e2fde34c8dfb855d
Varun Sukheja <>
2019-04-14 17:53:55 +0530

    hobby component completed: displays a hobby name and image

--- src/app/component/hobby/hobby.component.ts
+++ src/app/component/hobby/hobby.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
+import { hobbies } from './hobby.constant';
 @Component({
   selector: 'app-hobby',
   templateUrl: './hobby.component.html',
-  styleUrls: ['./hobby.component.scss']
+  styleUrls: ['./hobby.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HobbyComponent implements OnInit {
+
+  readonly hobbyImgList = hobbies;
+  @Input() hobby: string;
 
   constructor() { }
 
