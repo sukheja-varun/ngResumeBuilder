5c5409d630a115a4282a4b9ea7bbaefaae1fcb19
Sachin Bachhav <ssbachhav18@gmail.com>
2019-04-20 18:21:44 +0530

    conflict changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RadialProgressBarComponent } from './progress-bar/radial-progress-bar/radial-progress-bar.component';
 import { HobbyComponent } from './component/hobby/hobby.component';
-<<<<<<< HEAD
 import { TimelineComponent } from './component/timeline/timeline.component';
-=======
 import { LinearProgressBarComponent } from './progress-bar/linear-progress-bar/linear-progress-bar.component';
->>>>>>> Linear progress bar completed
 
 @NgModule({
   declarations: [
     AppComponent,
     RadialProgressBarComponent,
     HobbyComponent,
-<<<<<<< HEAD
-    TimelineComponent
-=======
+    TimelineComponent,
     LinearProgressBarComponent
->>>>>>> Linear progress bar completed
   ],
   imports: [
     BrowserModule,
