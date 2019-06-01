a1090f331976c984dfff424aaa1170c6f408ef99
Sachin Bachhav <ssbachhav18@gmail.com>
2019-06-02 02:08:23 +0530

    Box templet added

--- src/app/component/hobby/hobby.component.ts
+++ src/app/component/hobby/hobby.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 import { hobbies } from './hobby.constant';
 @Component({
   selector: 'app-hobby',
   templateUrl: './hobby.component.html',
-  styleUrls: ['./hobby.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./hobby.component.scss']
 })
 export class HobbyComponent implements OnInit {
 
--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Experience } from 'src/app/user-info/user-info.model';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
 
