66f9d7caaaf116a403335f6288e7d786d0851b7a
Varun Sukheja <>
2019-06-02 18:39:07 +0530

    created dummy form for filling user data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { Template1Component } from './template/template1/template1.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'template1', component: Template1Component }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'template1', component: Template1Component },
 ];
 
 @NgModule({
